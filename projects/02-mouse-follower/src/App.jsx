5cb79095e772e9e8d284ba349bf0e4c28607e443
emlez <emilio@trynow.io>
2023-01-28 23:54:25 -0600

    feat: Mouse follower

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,8 +1,50 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 function App () {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('useEffect', { enabled })
+
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      // console.log('handleMove', { clientX, clientY })
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <h3>3rd Project</h3>
+    <main>
+      <div
+        style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          borderRadius: '50%',
+          border: '1px solid #fff',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -20,
+          top: -20,
+          width: 40,
+          height: 40,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      />
+      <h3>3rd Project</h3>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Disable' : 'Enable'} mouse follower
+      </button>
+    </main>
   )
 }
 
